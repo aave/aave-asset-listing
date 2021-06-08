da27c4baf00107770360be56caef0069c1f62b40
Guillaume Felley <guillaume@felley.io>
2021-06-08 13:21:02 +0200

    Add RAI listing test

---
+++ test/test-listing-rai.spec.ts
@@ -0,0 +1,257 @@
+
+import path from 'path';
+import { expect } from 'chai';
+import { config } from 'dotenv';
+
+import rawBRE, { ethers } from 'hardhat';
+
+import { BigNumber } from 'ethers';
+import { parseEther } from 'ethers/lib/utils';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import { JsonRpcSigner } from '@ethersproject/providers';
+import {
+  evmSnapshot,
+  increaseTime,
+  evmRevert,
+  latestBlock,
+  advanceBlockTo,
+  impersonateAccountsHardhat,
+  MAX_UINT_AMOUNT,
+} from './utils/utils';
+import { parsePoolData } from './utils/listing';
+import { IAaveGovernanceV2 } from '../types/IAaveGovernanceV2';
+import { IAaveOracle } from '../types/IAaveOracle';
+import { ILendingPool } from '../types/ILendingPool';
+import { SelfdestructTransferFactory } from '../types/SelfdestructTransferFactory'
+import { IERC20 } from '../types/IERC20';
+
+config({ path: path.resolve(process.cwd(), '.rai.env') });
+
+const {
+  TOKEN,
+  ATOKEN,
+  STABLE_DEBT_TOKEN,
+  VARIABLE_DEBT_TOKEN,
+  INTEREST_STRATEGY,
+  LTV,
+  LIQUIDATION_THRESHOLD,
+  LIQUIDATION_BONUS,
+  RESERVE_FACTOR,
+  DECIMALS,
+  IPFS_HASH,
+  AAVE_GOVERNANCE_V2 = '0xEC568fffba86c094cf06b22134B23074DFE2252c', // mainnet
+  AAVE_SHORT_EXECUTOR = '0xee56e2b3d491590b5b31738cc34d5232f378a8d5', // mainnet
+} = process.env;
+
+if (
+  !TOKEN ||
+  !ATOKEN ||
+  !STABLE_DEBT_TOKEN ||
+  !VARIABLE_DEBT_TOKEN ||
+  !INTEREST_STRATEGY ||
+  !LTV ||
+  !LIQUIDATION_BONUS ||
+  !LIQUIDATION_THRESHOLD ||
+  !DECIMALS ||
+  !IPFS_HASH ||
+  !AAVE_GOVERNANCE_V2 ||
+  !AAVE_SHORT_EXECUTOR ||
+  !RESERVE_FACTOR
+) {
+  throw new Error('You have not set correctly the .env file, make sure to read the README.md');
+}
+
+const AAVE_LENDING_POOL = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9';
+const VOTING_DURATION = 19200;
+
+const AAVE_WHALE = '0x25f2226b597e8f9514b3f68f00f494cf4f286491';
+const AAVE_TOKEN = '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9';
+
+const RAI_HOLDER = '0x1C051112075FeAEe33BCDBe0984C2BB0DB53CF47';
+// const AAVE_ORACLE_OWNER = '0xb9062896ec3a615a4e4444df183f0531a77218ae';
+// const AAVE_ORACLE = '0xa50ba011c48153de246e5192c8f9258a2ba79ca9';
+
+const DAI_TOKEN = '0x6b175474e89094c44da98b954eedeac495271d0f';
+const DAI_HOLDER = '0x72aabd13090af25dbb804f84de6280c697ed1150';
+
+const ERRORS = {
+  NO_BORROW: '7',
+  NO_COLLATERAL_BALANCE: '9',
+  NO_STABLE_BORROW: '12',
+};
+
+describe('Deploy RAI assets with different params', () => {
+  let whale: JsonRpcSigner;
+  let raiHolder: JsonRpcSigner;
+  let daiHolder: JsonRpcSigner;
+  let proposer: SignerWithAddress;
+  let gov: IAaveGovernanceV2;
+  let pool: ILendingPool;
+  let oracle: IAaveOracle;
+  let aave: IERC20;
+  let rai: IERC20;
+  let dai: IERC20;
+  let aRai: IERC20;
+  let stableDebt: IERC20;
+  let variableDebt: IERC20;
+  let proposal: BigNumber;
+  let snapshotId: string;
+  let decimalMultiplier: BigNumber;
+  before(async () => {
+    [proposer] = await rawBRE.ethers.getSigners();
+    // send ether to the AAVE_WHALE, which is a non payable contract. Via selfdestruct
+    await rawBRE.deployments.deploy('SelfdestructTransfer', { from: proposer.address });
+    let selfDestructContract = await new SelfdestructTransferFactory(proposer).deploy();
+    await (
+      await selfDestructContract.destroyAndTransfer(AAVE_WHALE, {
+        value: ethers.utils.parseEther('1'),
+      })
+    ).wait();
+    selfDestructContract = await new SelfdestructTransferFactory(proposer).deploy();
+    await (
+      await selfDestructContract.destroyAndTransfer(RAI_HOLDER, {
+        value: ethers.utils.parseEther('1'),
+      })
+    ).wait();
+    await impersonateAccountsHardhat([AAVE_WHALE, RAI_HOLDER, DAI_HOLDER]);
+
+    // impersonating holders
+
+    whale = ethers.provider.getSigner(AAVE_WHALE);
+    raiHolder = ethers.provider.getSigner(RAI_HOLDER);
+    daiHolder = ethers.provider.getSigner(DAI_HOLDER);
+    //getting main entry point contracts
+    gov = (await ethers.getContractAt(
+      'IAaveGovernanceV2',
+      AAVE_GOVERNANCE_V2,
+      proposer
+    )) as IAaveGovernanceV2;
+    pool = (await ethers.getContractAt(
+      'ILendingPool',
+      AAVE_LENDING_POOL,
+      proposer
+    )) as ILendingPool;
+
+    // getting tokens used for tests
+    aave = (await ethers.getContractAt('IERC20', AAVE_TOKEN, whale)) as IERC20;
+    dai = (await ethers.getContractAt('IERC20', DAI_TOKEN, daiHolder)) as IERC20;
+    rai = (await ethers.getContractAt('IERC20', TOKEN, raiHolder)) as IERC20;
+    decimalMultiplier = BigNumber.from('10').pow(await rai.decimals());
+    
+    // Give rai to whale
+    await (
+      await aave.transfer(
+        proposer.address,
+        (await aave.balanceOf(AAVE_WHALE)).sub(parseEther('10000'))
+      )
+    ).wait();
+
+    // giving just a bit of Dai to RAI holder to pay for interest later
+    await (await dai.transfer(RAI_HOLDER, parseEther('10'))).wait();
+    await (
+      await rai.transfer(
+        proposer.address,
+        (await rai.balanceOf(RAI_HOLDER)).sub((parseEther('1000').div(decimalMultiplier)))
+      )
+    ).wait();
+
+
+    // deploying the payload
+    await rawBRE.ethers.provider.send('evm_mine', [0]);
+    
+    await rawBRE.deployments.deploy('AssetListingProposalGenericExecutor', {
+      from: proposer.address,
+      gasLimit: 4000000,
+      gasPrice: BigNumber.from('75000000000'),
+      args: [],
+    });
+
+
+    proposal = await gov.getProposalsCount();
+    
+    await rawBRE.run('create:proposal-new-asset');
+
+    console.log("YES")
+
+    // voting, queuing proposals
+    await rawBRE.ethers.provider.send('evm_mine', [0]);
+
+    await (await gov.submitVote(proposal, true)).wait();
+    await advanceBlockTo((await latestBlock()) + VOTING_DURATION + 1);
+    await (await gov.queue(proposal)).wait();
+    let proposalState = await gov.getProposalState(proposal);
+    expect(proposalState).to.be.equal(5);
+    await increaseTime(86400 + 10);
+    snapshotId = await evmSnapshot();
+  });
+  
+  it('Should list correctly an asset: borrow on, collateral off, stable borrow off', async () => {
+
+    await (await gov.execute(proposal)).wait();
+    const proposalState = await gov.getProposalState(proposal);
+    expect(proposalState).to.be.equal(7);
+    const {
+      configuration: { data },
+      aTokenAddress,
+      stableDebtTokenAddress,
+      variableDebtTokenAddress,
+    } = await pool.getReserveData(TOKEN);
+    const poolData = parsePoolData(data);
+    expect(poolData).to.be.eql({
+      reserveFactor: RESERVE_FACTOR,
+      reserved: '0',
+      stableRateEnabled: '0',
+      borrowingEnabled: '1',
+      reserveFrozen: '0',
+      reserveActive: '1',
+      decimals: DECIMALS,
+      liquidityBonus: '0',
+      LiquidityThreshold: '0',
+      LTV: '0',
+    });
+
+    // preparing for tests.
+    aRai = (await ethers.getContractAt('IERC20', aTokenAddress, proposer)) as IERC20;
+    stableDebt = (await ethers.getContractAt('IERC20', stableDebtTokenAddress, proposer)) as IERC20;
+    variableDebt = (await ethers.getContractAt(
+      'IERC20',
+      variableDebtTokenAddress,
+      proposer
+    )) as IERC20;
+    await (await rai.connect(raiHolder).approve(pool.address, parseEther('200000'))).wait();
+    await (await aave.connect(proposer).approve(pool.address, parseEther('200000'))).wait();
+
+    // AAVE deposit by proposer
+    await (await pool.deposit(aave.address, parseEther('100'), proposer.address, 0)).wait();
+    // AMPL deposit by ampl holder
+    const depositedAmount = parseEther('100').div(decimalMultiplier);
+    await (
+      await pool.connect(raiHolder).deposit(rai.address, depositedAmount, RAI_HOLDER, 0)
+    ).wait();
+    expect(await aRai.balanceOf(RAI_HOLDER)).to.gte(depositedAmount.sub(1));
+    expect(await aRai.balanceOf(RAI_HOLDER)).to.lte(depositedAmount.add(1));
+
+    // AMPL holder not able to borrow DAI against AMPL
+    await expect(
+      pool.connect(raiHolder).borrow(dai.address, parseEther('1'), 2, 0, RAI_HOLDER)
+    ).to.be.revertedWith(ERRORS.NO_COLLATERAL_BALANCE);
+
+    // proposer able to borrow AMPL variable against AAVE
+    const borrowAmount = parseEther('10').div(decimalMultiplier);
+    await (
+      await pool.connect(proposer).borrow(rai.address, borrowAmount, 2, 0, proposer.address)
+    ).wait();
+    expect(await variableDebt.balanceOf(proposer.address)).to.be.equal(borrowAmount);
+
+    // proposer not able to borrow AMPL stable against AAVE
+    await expect(
+      pool.borrow(rai.address, borrowAmount, 1, 0, proposer.address)
+    ).to.be.revertedWith(ERRORS.NO_STABLE_BORROW);
+    increaseTime(40000);
+
+    // proposer able to repay AMPL variable
+    await (await rai.connect(proposer).approve(pool.address, parseEther('100000'))).wait();
+    await (await pool.repay(rai.address, MAX_UINT_AMOUNT, 2, proposer.address)).wait();
+    expect(await variableDebt.balanceOf(proposer.address)).to.be.equal(parseEther('0'));
+  });
+});

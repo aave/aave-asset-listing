569858afd287cf08f2b8e3615e9458aaa6b4239a
kassandra <0xdada@protonmail.com>
2021-11-25 08:05:59 -0500

    add listing tasks and test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,6 +13,8 @@ if (process.env.SKIP_LOAD !== 'true') {
   require('./tasks/list-new-asset.ts');
   require('./tasks/list-rai.ts');
   require('./tasks/list-bond.ts');
+  require('./tasks/list-gunidaiusdc.ts');
+  require('./tasks/list-guniusdcusdt.ts');
 }
 
 export const BUIDLEREVM_CHAIN_ID = 31337;
